0ce9b27e0ca52db5a38a5df328208389a87d1c00
moonfdd <3182319461@qq.com>
2024-01-06 13:07:14 +0800

    power_mod

--- examples/a001_ecdlp_base/b001_power_mod/main.go
+++ examples/a001_ecdlp_base/b001_power_mod/main.go
@@ -13,7 +13,7 @@ func main() {
 	}
 	if true {
 		// Exp就是求快速幂
-		r := big.NewInt(5).Exp(big.NewInt(5), big.NewInt(3), big.NewInt(7))
+		r := big.NewInt(0).Exp(big.NewInt(5), big.NewInt(3), big.NewInt(7))
 		fmt.Println(r)
 	}
 	fmt.Println("")
@@ -24,16 +24,16 @@ func main() {
 // b是非负整数
 // p是大于等于3的质数
 func PowerMod(a, b, p *big.Int) (ans *big.Int) {
-	ans = big.NewInt(1)             //ans=1
-	a.Mod(a, p)                     //a=a%p
+	ans = big.NewInt(1)         //ans=1
+	a = big.NewInt(0).Mod(a, p) //a=a%p
+	b = big.NewInt(0).Set(b)    //循环里会改变b的值
+	// b.Mod(b, big.NewInt(0).Sub(p, big.NewInt(1))) //b=b%(p-1)
 	for b.Cmp(big.NewInt(0)) != 0 { //b!=0
-		if big.NewInt(0).And(b, big.NewInt(1)).Cmp(big.NewInt(0)) != 0 { //b&1!=0
-			ans.Mul(ans, a) //ans*=a
-			ans.Mod(ans, p) //ans%=p
+		if b.Bit(0) == 1 { //b&1!=0
+			ans.Mul(ans, a).Mod(ans, p) //ans*=a ans%=p
 		}
-		b.Rsh(b, 1) //b>>=1
-		a.Mul(a, a) //a*=a
-		a.Mod(a, p) //a%=p
+		b.Rsh(b, 1)           //b>>=1
+		a.Mul(a, a).Mod(a, p) //a*=a a%=p
 	}
 	//返回ans
 	return
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/moonfdd/ecdlp
 go 1.16
 
 require (
-	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
-	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/ncw/gmp v1.0.5 // indirect
 )
